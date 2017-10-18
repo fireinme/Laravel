53a384e23d8f99a8fadc0b4f2ce49243e1a7d716
dennis <1016144734grc@gmail.com>
2017-10-18 16:17:26 +0800

    主要完成了发送邮箱功能，不清不楚

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,10 @@ Route::resource('users', 'UsersController');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
-//计划邮箱
+//激活邮箱
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+//找回密码功能
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
